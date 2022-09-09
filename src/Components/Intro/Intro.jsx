f73f3a2f74f33837ec7b4e22d9e97ec6883e3c55
avinashjha1320 <jhaavinash0118@gmail.com>
2022-09-09 12:29:40 +0530

    today

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -1,50 +1,97 @@
-import React from 'react'
-import './Intro.css'
-import Github from '../../img/github.png';
-import LinkedIn from '../../img/linkedin.png';
-import Instagram from '../../img/instagram.png';
+import React, { useContext } from "react";
+import "./Intro.css";
 import Vector1 from "../../img/Vector1.png";
 import Vector2 from "../../img/Vector2.png";
 import boy from "../../img/boy.png";
 import glassesimoji from "../../img/glassesimoji.png";
 import thumbup from "../../img/thumbup.png";
 import crown from "../../img/crown.png";
-import FloatingDiv from '../FloatingDiv/FloatingDiv';
-
+import FloatinDiv from "../FloatingDiv/FloatingDiv";
+import Github from "../../img/github.png";
+import LinkedIn from "../../img/linkedin.png";
+import Instagram from "../../img/instagram.png";
+import { themeContext } from "../../Context";
+import { motion } from "framer-motion";
+import { Link } from "react-scroll";
 const Intro = () => {
+  // Transition
+  const transition = { duration: 2, type: "spring" };
+
+  // context
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+
   return (
-    <div className="intro">
+    <div className="Intro" id="Intro">
+      {/* left name side */}
       <div className="i-left">
         <div className="i-name">
-          <span>Hy! I Am</span>
-          <span>Avinash Jha</span>
-          <span>I am a Aspiring Product Manager with a tech background and experince in development</span>
+          {/* yahan change hy darkmode ka */}
+          <span style={{ color: darkMode ? "white" : "" }}>Hy! I Am</span>
+          <span>Andrew Thomas</span>
+          <span>
+            Frontend Developer with high level of experience in web designing
+            and development, producting the Quality work
+          </span>
         </div>
-        <button className="button i-button">Contact Me</button>
+        <Link to="contact" smooth={true} spy={true}>
+          <button className="button i-button">Hire me</button>
+        </Link>
+        {/* social icons */}
         <div className="i-icons">
-          <a href="https://github.com/avinashjha1320" target="-blank"><img src={Github} alt="" /></a>
-
+          <img src={Github} alt="" />
           <img src={LinkedIn} alt="" />
           <img src={Instagram} alt="" />
         </div>
       </div>
+      {/* right image side */}
       <div className="i-right">
         <img src={Vector1} alt="" />
         <img src={Vector2} alt="" />
         <img src={boy} alt="" />
-        <img src={glassesimoji} alt="" />
-        <div  style={{top:'-4%', left:'68%'}}>
-          <FloatingDiv image={crown} txt1='Web' txt2='Development'/>
-        </div>
-        <div  style={{top:'18rem', left:'0rem'}}>
-          <FloatingDiv image={thumbup} txt1='Product' txt2='Management'/>
-        </div>
-        {/* Blur Div */}
-        <div className="blur" style={{background:"rgb(238 210 255"}}></div>
-        <div className="blur" style={{background:'#C1F5FF', top:'17rem', width:'21rem', height:'11rem', left:'-9rem'}}></div>
+        {/* animation */}
+        <motion.img
+          initial={{ left: "-36%" }}
+          whileInView={{ left: "-24%" }}
+          transition={transition}
+          src={glassesimoji}
+          alt=""
+        />
+
+        <motion.div
+          initial={{ top: "-4%", left: "74%" }}
+          whileInView={{ left: "68%" }}
+          transition={transition}
+          className="floating-div"
+        >
+          <FloatinDiv img={crown} text1="Web" text2="Developer" />
+        </motion.div>
+
+        {/* animation */}
+        <motion.div
+          initial={{ left: "9rem", top: "18rem" }}
+          whileInView={{ left: "0rem" }}
+          transition={transition}
+          className="floating-div"
+        >
+          {/* floatinDiv mein change hy dark mode ka */}
+          <FloatinDiv img={thumbup} text1="Best Design" text2="Award" />
+        </motion.div>
+
+        <div className="blur" style={{ background: "rgb(238 210 255)" }}></div>
+        <div
+          className="blur"
+          style={{
+            background: "#C1F5FF",
+            top: "17rem",
+            width: "21rem",
+            height: "11rem",
+            left: "-9rem",
+          }}
+        ></div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Intro
+export default Intro;
